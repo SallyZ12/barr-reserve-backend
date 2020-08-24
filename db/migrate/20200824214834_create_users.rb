a00c43cc8968b5c86c3989bc682f20c4091a0f1e
SallyZ12 <szasloff@gmail.com>
2020-08-24 17:59:06 -0400

    add email before migrated

--- db/migrate/20200824214834_create_users.rb
+++ db/migrate/20200824214834_create_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :first_name
       t.string :last_name
       t.string :username
+      t.string :email
       t.string :apartment
       t.string :password_digest
       t.string :admin
