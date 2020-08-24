9b91f446078802fbbf0671353ce28a346d6d7476
SallyZ12 <szasloff@gmail.com>
2020-08-24 18:06:31 -0400

    changed password attribute to password_digest to load some data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.create(first_name: 'Bobo', last_name: 'Jazz', username: 'BJ', email: "bj@email.com", apartment: "PHB", password_digest: "password", admin: "No")
+user2 = User.create(first_name: 'Waldo', last_name: 'Where', username: 'WW', email: "ww@email.com", apartment: "5D", password_digest: "password", admin: "No")
