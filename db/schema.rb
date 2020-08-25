c0cd3545e40aae2c335ce81085ed0d8b6b2ef28b
SallyZ12 <szasloff@gmail.com>
2020-08-25 17:37:47 -0400

    created resources for Room

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_214834) do
+ActiveRecord::Schema.define(version: 2020_08_25_213620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "room_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
