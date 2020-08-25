6f9e9cdab06a0fb08032af4e60183aa79da307d1
SallyZ12 <szasloff@gmail.com>
2020-08-25 18:15:08 -0400

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,11 @@
 
 user1 = User.create(first_name: 'Bobo', last_name: 'Jazz', username: 'BJ', email: "bj@email.com", apartment: "PHB", password_digest: "password", admin: "No")
 user2 = User.create(first_name: 'Waldo', last_name: 'Where', username: 'WW', email: "ww@email.com", apartment: "5D", password_digest: "password", admin: "No")
+
+
+room1 = Room.create(room_name: "Gym")
+room2 = Room.create(room_name: "Club Room")
+
+reservation1 = Reservation.create(user_id: 1, room_id: 1, hour: '12:00 pm', date: '2020-9-3', confirmID: 123456)
+reservation2 = Reservation.create(user_id: 1, room_id: 1, hour: '2:00 pm', date: '2020-9-3', confirmID: 123333)
+reservation3 = Reservation.create(user_id: 2, room_id: 2, hour: '8:00 pm', date: '2020-9-4', confirmID: 123456)
