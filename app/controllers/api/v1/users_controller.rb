391ce586e949e037dc10ea35c90421d20221963b
SallyZ12 <szasloff@gmail.com>
2020-08-28 15:25:07 -0400

    added show action and private methods set_user and user_params to users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,4 +6,25 @@ class Api::V1::UsersController < ApplicationController
   end
 
 
+  def show
+    # if logged_in?
+      @user = set_user
+
+    render json: @user
+  end
+
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :username, :email, :apartment, :password, :admin)
+    end
+
+
+
+
 end
