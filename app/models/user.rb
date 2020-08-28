8ebb4d5ec513ce8118d6684b4136ee110529826f
SallyZ12 <szasloff@gmail.com>
2020-08-28 14:49:37 -0400

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
   has_many :reservations
   has_many :rooms, through: :reservations
 
+  validates :first_name, :last_name, :username, :email, :apartment, presence: true
+  validates :username, :email, uniqueness: true
+
+  validates :password, presence: true, length: {minimum: 8}
+
 end
