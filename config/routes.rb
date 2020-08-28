34fd47c5e4b534a4ded5e893f624155b5861d678
SallyZ12 <szasloff@gmail.com>
2020-08-28 15:13:43 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,37 @@
 Rails.application.routes.draw do
-  resources :reservations
-  resources :rooms
-  resources :users
+
+
+  post "api/v1/signup", to: "api/v1/users#create"
+  post "/api/v1/login", to: "api/v1/sessions#create"
+  get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
+  delete "/api/v1/logout", to: "api/v1/sessions#destroy"
+
+
+  namespace :api do
+     namespace :v1 do
+       # resources :reservations
+       resources :users
+       resources :rooms
+     end
+   end
+
+   namespace :api do
+         namespace :v1 do
+             resources :users do
+               resources :reservations
+       end
+     end
+   end
+
+   namespace :api do
+       namespace :v1 do
+         resources :rooms do
+           resources :reservations
+         end
+       end
+     end
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
