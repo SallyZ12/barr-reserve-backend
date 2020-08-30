07be48c8cf512275955faca1c2dafdeb74b01c95
SallyZ12 <szasloff@gmail.com>
2020-08-30 15:07:20 -0400

    added resources: reservations to test ddata in browser

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   namespace :api do
      namespace :v1 do
-       # resources :reservations
+       resources :reservations
        resources :users
        resources :rooms
      end
