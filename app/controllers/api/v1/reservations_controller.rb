a1be9c8aa498c70ca1ffb07133bee7a902d6227c
SallyZ12 <szasloff@gmail.com>
2020-09-04 11:06:15 -0400

    added private methods set_reservations set_room set_user and reservation_params

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -5,4 +5,36 @@ class Api::V1::ReservationsController < ApplicationController
        render json: @reservations
   end
 
+
+
+
+  private
+
+    def set_reservation
+      @reservation = Reservation.find(params[:id])
+    end
+  
+
+    def set_room
+      @room = Room.find(params[:room_id])
+    end
+
+    def set_user
+      @user = User.find(params[:user_id])
+    end
+
+
+    def reservation_params
+      params.require(:reservation).permit(:user_id,:room_id, :hour, :date, :confirmID)
+    end
+
+
+
+
+
+
+
+
+
+
 end
