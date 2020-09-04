7c0f3bf6ba4891ad9bef479ff579146b8f7b5b77
SallyZ12 <szasloff@gmail.com>
2020-09-04 12:37:28 -0400

    commented out method reservation_room

--- app/serializers/reservation_serializer.rb
+++ app/serializers/reservation_serializer.rb
@@ -1,11 +1,11 @@
 class ReservationSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :room_id, :hour, :date, :confirmID, :reservation_room
+  attributes :id, :user_id, :room_id, :hour, :date, :confirmID
 
   belongs_to :user
 
-  def reservation_room
-    self.object.room.room_name
-  end
+  # def reservation_room
+  #   self.object.room.room_name
+  # end
 
 
 
