589f00179cb6e100549b29b011acf112d8c36985
SallyZ12 <szasloff@gmail.com>
2020-09-20 12:48:17 -0400

    added include ActionController for Coookies and Helpers, current_player and logged_in actions and helper methods for current_player and logged_in?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::API
+    include ::ActionController::Cookies
+    include ::ActionController::Helpers
+
+    helper_method :current_player
+    helper_method :logged_in?
+
+
+    def current_player
+      User.find_by(id: session[:user_id])
+    end
+
+
+     def logged_in?
+       !!current_player
+     end
+
+
+
+
 end
