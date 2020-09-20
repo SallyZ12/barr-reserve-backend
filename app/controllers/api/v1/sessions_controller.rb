17a1e2dee15cbe02ce0ba97ff4c8b84b87e0288d
SallyZ12 <szasloff@gmail.com>
2020-09-20 12:43:25 -0400

    added create, get_current_user, and desttroy actions

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,37 @@
+class Api::V1::SessionsController < ApplicationController
+
+  def create
+
+    @user = User.find_by(username: params[:session][:username])
+
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+
+      render json: @user, status: :ok
+    else
+      render json: {
+        error: "Invalid Username and/or Password"
+      }
+    end
+  end
+
+  def get_current_user
+    if logged_in?
+      render json: current_player
+    else
+      render json: {
+        error: "Please Log In to Reserve the Room"
+      }
+    end
+  end
+
+  def destroy
+    session.clear
+    render json: {
+      notice: "successfully logged out"
+    }, status: :ok
+  end
+
+
+
+end
