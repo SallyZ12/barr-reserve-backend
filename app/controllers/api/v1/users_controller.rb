1a028a8bb1fcb97f087768c35932fa50df61f397
SallyZ12 <szasloff@gmail.com>
2020-09-24 19:21:21 -0400

    added create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,6 +13,33 @@ class Api::V1::UsersController < ApplicationController
     render json: @user
   end
 
+  def create
+
+    @user = User.new(user_params)
+      if params[:user][:admin] === 'No' || is_admin === true
+
+            if @user.save
+              session[:user_id] = @user.id
+              render json: @user, status: :created
+            else
+              resp = {
+              error: @user.errors.full_messages.to_sentence
+            }
+            render json: resp
+            end
+      else
+        render json: {
+        error: "Admin exists -- only one permitted"
+        }
+    end
+end
+
+
+
+
+
+
+
 
   private
 
