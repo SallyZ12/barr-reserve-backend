5dbc6e647e7d5cd021c526b8d65bc49fc586e1fb
SallyZ12 <szasloff@gmail.com>
2020-09-26 13:11:19 -0400

    in create action changed is_admin === false from true

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::UsersController < ApplicationController
 
+  before_action :set_user, only: [:show, :update, :destroy]
+
   def index
     @users = User.all
     render json: @users
@@ -16,7 +18,8 @@ class Api::V1::UsersController < ApplicationController
   def create
 
     @user = User.new(user_params)
-      if params[:user][:admin] === 'No' || is_admin === true
+    # binding.pry
+      if params[:user][:admin] === 'No' || is_admin === false
 
             if @user.save
               session[:user_id] = @user.id
@@ -27,6 +30,7 @@ class Api::V1::UsersController < ApplicationController
             }
             render json: resp
             end
+
       else
         render json: {
         error: "Admin exists -- only one permitted"
