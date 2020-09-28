2f926e9eafac8257168cf13b774008f3452712c7
SallyZ12 <szasloff@gmail.com>
2020-09-28 11:12:57 -0400

    formatting

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,11 @@ Rails.application.routes.draw do
 
 
   namespace :api do
-     namespace :v1 do
-       resources :users
-       resources :rooms
+   namespace :v1 do
+    resources :users
+      resources :rooms
      end
-   end
+  end
 
    namespace :api do
          namespace :v1 do
