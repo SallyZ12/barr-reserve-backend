fc23aa4b8aec4d9b80dfa4817bd4950436ef12d0
SallyZ12 <szasloff@gmail.com>
2020-10-04 14:39:36 -0400

    added create, update, destroy actions, and private methods

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -13,6 +13,47 @@ class Api::V1::RoomsController < ApplicationController
 
      end
 
+     def create
+      @room = Room.new(room_params)
+           if @room.save
+           render json: @room
+           else
+           resp = {
+             error: @room.errors.full_messages.to_sentence
+           }
+       render json: resp
+        end
+    end
+
+    def update
+      @room = set_room
+
+         @room.update(room_params)
+         if @room.save
+             render json: @room
+         else
+           resp = {
+             error: @user.errors.full_messages.to_sentence
+           }
+           render json: resp
+         end
+     end
+
+    def destroy
+          @room = set_room
+
+          if @room.reservations.present?
+            render json: {
+              error: "Can not delete a Room with reservations"
+         }
+
+         else
+          @room.destroy
+          render json: @room
+        end
+    end
+
+
 
      private
 
@@ -20,11 +61,17 @@ class Api::V1::RoomsController < ApplicationController
          @room = User.find(params[:id])
        end
 
-       def user_params
+       def room_params
          params.require(:user).permit(:room_name)
        end
 
+       def set_user
+       @user = User.find(params[:user_id])
+      end
 
+      def set_reservation
+       @reservation = Reservation.find(params[:reservation_id])
+     end
 
 
 
