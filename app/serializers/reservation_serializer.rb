1cc9e052e1cf985ed439a986a758dd928769a74c
SallyZ12 <szasloff@gmail.com>
2020-10-19 12:50:16 -0400

    no changed, testing a method for reservation room name

--- app/serializers/reservation_serializer.rb
+++ app/serializers/reservation_serializer.rb
@@ -2,7 +2,7 @@ class ReservationSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :room_id, :hour, :date, :confirmID
 
   belongs_to :user
-  belongs_to :room  
+  belongs_to :room
 
   # def reservation_room
   #   self.object.room.room_name
